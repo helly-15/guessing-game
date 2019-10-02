4699181c5f4b6014e95d3e0fd2ed882f55e85876
Lenocka <lvutina.en@gmail.com>
2019-10-02 11:35:55 +0300

    feat:don task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+        this.guessTry = 0;
+    }
 
     setRange(min, max) {
+        this.minRange=min;
+        this.maxRange=max;
 
     }
 
     guess() {
+        this.guessTry = Math.ceil((this.minRange+this.maxRange)/2);
+        return this.guessTry;
 
     }
 
     lower() {
+        this.maxRange=this.guessTry;
 
     }
 
     greater() {
+        this.minRange=this.guessTry;
 
     }
 }
